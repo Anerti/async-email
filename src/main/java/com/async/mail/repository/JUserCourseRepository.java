acb89b53c21ffb4e3d1b6206db8964db24f83018
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-07-06 17:55:04 +0300

    style: remove unused Modifying import

--- src/main/java/com/async/mail/repository/JUserCourseRepository.java
+++ src/main/java/com/async/mail/repository/JUserCourseRepository.java
@@ -5,7 +5,6 @@ import java.time.Instant;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
