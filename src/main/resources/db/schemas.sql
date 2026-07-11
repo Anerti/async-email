4ea00fe7aa28859c791fd06f8cd5f4c0c7a2febb
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-07-11 18:55:36 +0300

    fix: make schema tables idempotent with IF NOT EXISTS

--- src/main/resources/db/schemas.sql
+++ src/main/resources/db/schemas.sql
@@ -6,7 +6,7 @@ BEGIN
 END
 $$;
 
-CREATE TABLE "user" (
+CREATE TABLE IF NOT EXISTS "user" (
     id          UUID PRIMARY KEY,
     first_name  VARCHAR(100) NOT NULL,
     last_name   VARCHAR(100) NOT NULL,
@@ -17,14 +17,14 @@ CREATE TABLE "user" (
     UNIQUE (first_name, last_name)
 );
 
-CREATE TABLE course (
+CREATE TABLE IF NOT EXISTS course (
     id         UUID PRIMARY KEY,
     title      VARCHAR(100) NOT NULL,
     start_date TIMESTAMP    NOT NULL,
     end_date   TIMESTAMP    NOT NULL
 );
 
-CREATE TABLE user_course (
+CREATE TABLE IF NOT EXISTS user_course (
     id             UUID PRIMARY KEY,
     user_id        UUID      NOT NULL REFERENCES "user"(id) ON DELETE CASCADE,
     course_id      UUID      REFERENCES course(id) ON DELETE SET NULL,
