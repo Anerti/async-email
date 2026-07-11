192d271b7d2a73d92e3085654a4e18194c778a87
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-07-11 19:04:52 +0300

    fix: make format.sh respect JAVA_HOME
    google-java-format 1.23.0 is incompatible with JDK 26. Use
    $JAVA_HOME when set, falling back to system java.

--- format.sh
+++ format.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-find src -name "*.java" -print0 | xargs -0 -n 500 java -jar google-java-format-1.23.0-all-deps.jar --replace
+JAVA="${JAVA_HOME:+$JAVA_HOME/bin/}java"
+find src -name "*.java" -print0 | xargs -0 -n 500 "$JAVA" -jar google-java-format-1.23.0-all-deps.jar --replace
