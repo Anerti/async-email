f3aed1a095e65e9585c201c9d1c08cab560bace9
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-07-11 16:59:47 +0300

    validator: add GeneralValidator and AuthValidator for input validation

---
+++ src/main/java/com/async/mail/validator/AuthValidator.java
@@ -0,0 +1,34 @@
+package com.async.mail.validator;
+
+import com.async.mail.endpoint.rest.controller.dto.SignUpRequest;
+import com.async.mail.exception.UnprocessableEntityException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AuthValidator {
+
+  private final GeneralValidator generalValidator;
+
+  public void validateSignUp(SignUpRequest request) {
+    generalValidator.checkNull("lastName", request.lastName());
+    generalValidator.validateName("lastName", request.lastName());
+
+    generalValidator.checkNull("firstName", request.firstName());
+    generalValidator.validateName("firstName", request.firstName());
+
+    generalValidator.validateUsername(request.username());
+
+    generalValidator.checkNull("email", request.email());
+    generalValidator.validateEmail(request.email());
+
+    generalValidator.checkPasswordSecurityLevel(request.password());
+
+    generalValidator.checkNull("confirmPassword", request.confirmPassword());
+
+    if (!request.password().equals(request.confirmPassword())) {
+      throw new UnprocessableEntityException("Passwords do not match.");
+    }
+  }
+}
