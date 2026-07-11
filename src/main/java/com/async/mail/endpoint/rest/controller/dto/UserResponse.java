671a03a3d83ca1f4adabfd789bff31402805f6bf
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-07-11 16:59:42 +0300

    dto: add SignUpRequest, AuthResponse, UserResponse

---
+++ src/main/java/com/async/mail/endpoint/rest/controller/dto/UserResponse.java
@@ -0,0 +1,7 @@
+package com.async.mail.endpoint.rest.controller.dto;
+
+import com.async.mail.entity.enums.UserRole;
+
+public record UserResponse(
+    java.util.UUID id, String firstName, String lastName, String username, String email,
+    UserRole role) {}
