85193808a694f1eab4c44f6a83038f160083b3e3
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-07-13 12:59:35 +0300

    fix: add missing double quote in format.sh nested substitution to fix shell syntax error

--- format.sh
+++ format.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-: "${JAVA_HOME:=$(dirname "$(dirname "$(readlink -f "$(which java)"))")}"
+: "${JAVA_HOME:=$(dirname "$(dirname "$(readlink -f "$(which java)")")")}"
 find src -name "*.java" -print0 | xargs -0 -n 500 "$JAVA_HOME/bin/java" -jar google-java-format-1.23.0-all-deps.jar --replace
