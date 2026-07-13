c16f3686a1f52179de462d3012308a4f4c608f31
Anerti <168828334+Anerti@users.noreply.github.com>
2026-07-13 08:29:03 +0300

    docs: POST /data and GET /data api specifications (#9)
    * poja: deployment ID: 71f9e33e-c4bd-42ad-b6df-4c6987e1020d
    
    * chore: add missing dependencies
    
    * feat: add Data entity with created_at field to canvas
    
    * feat: add GET/POST /data endpoints with pagination, filters, and ImageResponse schema
    
    ---------
    
    Co-authored-by: poja[bot] <bot@poja.io>

--- format.sh
+++ format.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-JAVA_HOME=${JAVA_HOME:-$HOME/.jdks/ms-21.0.11}
-find src -name "*.java" -print0 | xargs -0 -n 500 $JAVA_HOME/bin/java -jar google-java-format-1.25.2-all-deps.jar --replace
+find src -name "*.java" -print0 | xargs -0 -n 500 java -jar google-java-format-1.23.0-all-deps.jar --replace
